0662343dc9ecda47d004f020e5f56ce3d206e6af
Thenmozhi-Cheliyan <98871555+Thenmozhi-Cheliyan@users.noreply.github.com>
2022-05-16 15:10:14 +0530

    Trial Navigation Second Commit

--- app/src/main/java/com/example/trialnavigation/MainActivityTwo.kt
+++ app/src/main/java/com/example/trialnavigation/MainActivityTwo.kt
@@ -1,11 +1,24 @@
 package com.example.trialnavigation
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.databinding.DataBindingUtil
+import com.example.trialnavigation.databinding.ActivityMainBinding
+import com.example.trialnavigation.databinding.ActivityMainTwoBinding
 
 class MainActivityTwo : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main_two)
+//        setContentView(R.layout.activity_main_two)
+
+        val actionBar=supportActionBar
+        actionBar!!.title="Second Activity"
+        val binding = DataBindingUtil.setContentView<ActivityMainTwoBinding>(this,R.layout.activity_main_two)
+        binding.activityTwoButton.setOnClickListener{
+           val intent = Intent(this,MainActivityThree::class.java)
+            startActivity(intent)
+        }
     }
 }
